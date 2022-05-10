72e2035dbabbbb6badcf3637c986d50d2afabc9e
Caramel <goldrenard@gmail.com>
2022-05-10 21:30:13 +0300

    Initial project structure

--- MediaWatcherApp/Program.cs
+++ MediaWatcherApp/Program.cs
@@ -4,9 +4,11 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace MediaWatcherLib
+namespace MediaWatcherApp
 {
-    public class Class1
+    class Program
     {
+        static void Main(string[] args) {
+        }
     }
 }
